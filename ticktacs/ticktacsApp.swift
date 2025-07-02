bfa3a4b381929b4374c5c62981c268fa6a1ce1c8
Patryk <me@parpok.xyz>
2025-07-02 14:42:54 +0200

    playing around MacOS thingy

--- ticktacs/ticktacsApp.swift
+++ ticktacs/ticktacsApp.swift
@@ -5,8 +5,8 @@
 //  Created by Patryk Puciłowski on 25/06/2025.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct ticktacsApp: App {
@@ -14,6 +14,13 @@ struct ticktacsApp: App {
         WindowGroup {
             ContentView()
                 .modelContainer(for: Game.self)
+                #if os(macOS)
+                .containerBackground(.thinMaterial, for: .window)
+                .presentedWindowStyle(.hiddenTitleBar)
+                #endif
+            #if os(VisionOS)
+                .containerBackground(.thinMaterial, for: .navigation)
+            #endif
         }
     }
 }
